1fd0eba4270659044b10b93a68a84929f7d382cd
jemsley canela <jemscanela@gmail.com>
2018-05-23 16:43:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const app = "I don't do much."
 
-var kitten =["Milo", "Otis", "Garfield"]
+var kittens =["Milo", "Otis", "Garfield"]
 
 function kittens(){
   
-  var kitten =["Milo", "Otis", "Garfield"]
+  var kittens =["Milo", "Otis", "Garfield"]
 }
 
 function destructivelyAppendKitten(a){
-  kitten.push(a);
-  return kitten;
+  kittens.push(a);
+  return kittens;
 }
