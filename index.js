caad817e00d8f508d9c57c05ad0acf984020b31f
jemsley canela <jemscanela@gmail.com>
2018-05-23 16:38:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = "I don't do much."
 var kitten =["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name){
-  return kitten.push(name);
+  kitten.push(name);
+  return kitten;
 }
